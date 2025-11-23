d7b00fd595843c096b792fe848824e561b4c98c3
EmmanuelLefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-11-23 14:52:08 +0100

    fix get commit message bug + typos

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -914,7 +914,7 @@ function Test-LocalRepoExists {
   # Check if the path variable is defined AND if the folder exists on disk
   if (-not ($Path -and (Test-Path -Path $Path))) {
     # Helper called to center message nicely
-    $msg = "⚠️ Local repository path for $Name  doesn't exist ⚠️"
+    $msg = "⚠️ Local repository path for $Name doesn't exist ⚠️"
     $paddingStr = Get-CenteredPadding -RawMessage $msg
 
     # Display message
@@ -1183,9 +1183,9 @@ function Invoke-SafeCheckout {
     Write-Host -NoNewline "$TargetBranch" -ForegroundColor Red
     Write-Host " !!! ⚠️" -ForegroundColor Magenta
 
-    Write-Host -NoNewline "ℹ️ Blocked by local changes on " -ForegroundColor DarYellow
+    Write-Host -NoNewline "ℹ️ Blocked by local changes on " -ForegroundColor DarkYellow
     Write-Host -NoNewline "$OriginalBranch" -ForegroundColor Magenta
-    Write-Host ". Halting updates for this repository !" -ForegroundColor DarYellow
+    Write-Host ". Halting updates for this repository !" -ForegroundColor DarkYellow
 
     return $false
   }
@@ -1419,7 +1419,7 @@ function Show-LatestCommitMessage {
   }
 
   # Get new commits
-  $raw = git log --oneline --no-merges "$LocalBranch..$RemoteBranch" 2>$null
+  $raw = git log --pretty=format:"%s" --no-merges "$LocalBranch..$RemoteBranch" 2>$null
 
   # Normalisation : string → array
   $newCommits = @()
@@ -1444,18 +1444,11 @@ function Show-LatestCommitMessage {
     return
   }
 
-  # Cleanup if HideHashes option is enabled → removes hash in front of message
-  if ($HideHashes) {
-    $newCommits = $newCommits | ForEach-Object {
-      ($_ -replace '^[0-9a-f]+\s+', '')
-    }
-  }
-
   ######## GUARD CLAUSE : SINGLE COMMIT ########
   # One commit
   if ($newCommits.Count -eq 1) {
     Write-Host -NoNewline "Commit message : " -ForegroundColor Magenta
-    Write-Host "`"$($newCommits[0])`"" -ForegroundColor Cyan
+    Write-Host "  - `"$($newCommits[0])`"" -ForegroundColor Cyan
     return
   }
 
@@ -1463,7 +1456,7 @@ function Show-LatestCommitMessage {
   # Several commits
   Write-Host "New commits received :" -ForegroundColor Magenta
   foreach ($commit in $newCommits) {
-    Write-Host "- `"$commit`"" -ForegroundColor Cyan
+    Write-Host "  - `"$commit`"" -ForegroundColor DarkCyan
   }
 }
 
@@ -2034,7 +2027,7 @@ function Show-GitHubHttpError {
       Write-Host -NoNewline "⚠️ "
       Write-Host -NoNewline "Remote repository " -ForegroundColor Red
       Write-Host -NoNewline "$RepoName" -ForegroundColor white -BackgroundColor DarkBlue
-      Write-Host " doesn't exists ⚠️" -ForegroundColor Red
+      Write-Host " doesn't exist ⚠️" -ForegroundColor Red
       return
     }
 
